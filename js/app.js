577baa17938df50dd3977d888d8027282bf6629e
hasanrafi69 <hasanrafi69@gmail.com>
2022-02-17 13:36:14 +0600

    mandetory error handeling fix

--- js/app.js
+++ js/app.js
@@ -17,40 +17,44 @@ const remainingBalanceId = document.getElementById('remaining-balance');
 // Calculate Money
 function calculateMoney() {
     const income = parseFloat(incomeId.value);
-    console.log(typeof(income))
     const food = parseFloat(foodId.value);
-    console.log(typeof(food))
     const rent = parseFloat(rentId.value);
-    console.log(typeof(rent))
     const cloth = parseFloat(clothId.value);
-    console.log(typeof(cloth))
-    if (income == '' || typeof(income) != 'number') {
+    const totalExpense = food + rent + cloth;
+    const totalBalance = income - totalExpense;
+    if (income == '') {
         alert('Income Cant be Null')
     }
-    if (food == '' || typeof(food) != 'number') {
+    if (food == '') {
         alert('food Cant be Null')
     }
-    if (rent == '' || typeof(rent) != 'number') {
+    if (rent == '') {
         alert('rent Cant be Null')
     }
-    if (cloth == '' || typeof(cloth) != 'number') {
+    if (cloth == '') {
         alert('cloth insert number')
     }
-    if (typeof(income) == 'number' && typeof(food) == 'number' && typeof(rent) == 'number' && typeof(cloth) == 'number') {
-        const totalExpense = food + rent + cloth;
-        const totalBalance = income - totalExpense;
-        if (isNaN(totalExpense)) {
-            totalExpenseId.innerHTML = `<p class="ml-1 text-xl text-red-600 text-center">Please give all value as a numer</p > `;
-        } else {
-            totalExpenseId.innerText = totalExpense;
-        }
-        if (isNaN(totalBalance)) {
-            totalBalanceId.innerHTML = `<p class="ml-1 text-xl text-red-600 text-center">Please give all value as a numer</p > `;
+    debugger
+    if (totalExpense > income) {
+        totalBalanceId.innerHTML = `<p class="ml-1 text-xl text-red-600 text-center">You don't have enought money</p > `;
+    } else {
+        if (typeof(income) == 'number' && typeof(food) == 'number' && typeof(rent) == 'number' && typeof(cloth) == 'number') {
+            if (totalExpense > income) {
+                totalBalanceId.innerHTML = `<p class="ml-1 text-xl text-red-600 text-center">You don't have enought money</p > `;
+            }
+            if (isNaN(totalExpense)) {
+                totalExpenseId.innerHTML = `<p class="ml-1 text-xl text-red-600 text-center">Give all input value as a numer</p > `;
+            } else {
+                totalExpenseId.innerText = totalExpense;
+            }
+            if (isNaN(totalBalance)) {
+                totalBalanceId.innerHTML = `<p class="ml-1 text-xl text-red-600 text-center">Give all input value as a numer</p > `;
 
-        } else {
-            totalBalanceId.innerText = totalBalance;
-        }
+            } else {
+                totalBalanceId.innerText = totalBalance;
+            }
 
+        }
     }
 
 }
@@ -58,7 +62,6 @@ function calculateMoney() {
 // Save Money
 function saveMoney() {
     const income = parseFloat(incomeId.value);
-    console.log(incomeId.value);
     const food = parseFloat(foodId.value);
     const rent = parseFloat(rentId.value);
     const cloth = parseFloat(clothId.value);
@@ -70,11 +73,11 @@ function saveMoney() {
     totalSaveId.innerText = totalSaveAmount;
     remainingBalanceId.innerText = remainingBalance;
 
-    if (isNaN(totalSaveAmount)) {
-        totalSaveId.innerHTML = `<p class="ml-1 text-xl text-red-600 text-center">Please give toal and save value with numer</p>`;
+    if (isNaN(totalSaveAmount) || totalSaveAmount < 0) {
+        totalSaveId.innerHTML = `<p class="ml-1 text-xl text-red-600 text-center">Please give all input value and saving %  + value</p>`;
     }
     if (isNaN(remainingBalance)) {
-        remainingBalanceId.innerHTML = `<p class="ml-1 text-xl text-red-600 text-center">Please give all value as a numer</p>`;
+        remainingBalanceId.innerHTML = `<p class="ml-1 text-xl text-red-600 text-center">Please give all input value as a numer</p>`;
     }
 
 }
