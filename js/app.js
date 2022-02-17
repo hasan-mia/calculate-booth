a0ae0c20a940c27deca427214c43eb5e5c2e6549
hasanrafi69 <hasanrafi69@gmail.com>
2022-02-17 11:27:59 +0600

    fix some error

--- js/app.js
+++ js/app.js
@@ -17,28 +17,54 @@ const remainingBalanceId = document.getElementById('remaining-balance');
 // Calculate Money
 function calculateMoney() {
     const income = parseFloat(incomeId.value);
-    const food = foodId.value;
-    const rent = rentId.value;
-    const cloth = clothId.value;
-    const totalExpense = parseFloat(food) + parseFloat(rent) + parseFloat(cloth);
-    totalExpenseId.innerText = totalExpense;
-    const totalBalance = income - totalExpense;
-    totalBalanceId.innerText = totalBalance;
+    const food = parseFloat(foodId.value);
+    const rent = parseFloat(rentId.value);
+    const cloth = parseFloat(clothId.value);
+    const totalExpense = food + rent + cloth;
+    if (income == '' || typeof(income) != 'number') {
+        alert('Income Cant be Null')
+    }
+    if (food == '' || typeof(food) != 'number') {
+        alert('food Cant be Null')
+    }
+    if (rent == '' || typeof(rent) != 'number') {
+        alert('rent Cant be Null')
+    }
+    if (cloth == '' || typeof(cloth) != 'number') {
+        alert('cloth insert number')
+    }
+    if (typeof(income) == 'number' && typeof(food) == 'number' && typeof(rent) == 'number' && typeof(cloth) == 'number') {
+        totalExpenseId.innerText = totalExpense;
+        const totalBalance = income - totalExpense;
+        totalBalanceId.innerText = totalBalance;
+    } else {
+        totalExpenseId.innerHTML = `<p class="ml-1 text-xl text-red-600 text-center">Please give all value as a numer</p>`;
+        totalBalanceId.innerHTML = `<p class="ml-1 text-xl text-red-600 text-center">Please give all value as a numer</p>`;
+    }
+
+
+
 }
 
 // Save Money
 function saveMoney() {
     const income = parseFloat(incomeId.value);
-    const food = foodId.value;
-    const rent = rentId.value;
-    const cloth = clothId.value;
-    const totalExpense = parseFloat(food) + parseFloat(rent) + parseFloat(cloth);
+    console.log(incomeId.value);
+    const food = parseFloat(foodId.value);
+    const rent = parseFloat(rentId.value);
+    const cloth = parseFloat(clothId.value);
+    const totalExpense = food + rent + cloth;
     const saveAmount = parseFloat(saveAmountId.value) / 100;
     const totalSaveAmount = income * saveAmount;
-    totalSaveId.innerText = totalSaveAmount;
-    const totalBalance = income - totalExpense;
-    const remainingBalance = totalBalance - totalSaveAmount
-    remainingBalanceId.innerText = remainingBalance;
+    if (typeof(saveAmountId.value) == 'number' && typeof(income) == 'number' && typeof(food) == 'number' && typeof(rent) == 'number' && typeof(cloth) == 'number') {
+        totalSaveId.innerText = totalSaveAmount;
+        const totalBalance = income - totalExpense;
+        const remainingBalance = totalBalance - totalSaveAmount
+        remainingBalanceId.innerText = remainingBalance;
+    } else {
+        totalSaveId.innerHTML = `<p class="ml-1 text-xl text-red-600 text-center">Please give all value as a numer</p>`;
+    }
+
 }
 
 // Calculate Button
